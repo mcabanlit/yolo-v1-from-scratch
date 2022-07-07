273b599ebc85b1d862d352c6830f68b7b8cb5afe
mcabanlit <102983286+mcabanlit@users.noreply.github.com>
2022-07-07 11:03:53 +0800

    Created loss function

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 # https://www.youtube.com/watch?v=n9_XyCGr-MI
 # https://arxiv.org/pdf/1506.02640.pdf
+# https://github.com/aladdinpersson/Machine-Learning-Collection/tree/master/ML/Pytorch/object_detection/YOLO
 
 architecture_config = [
     # Kernel Size, Number of Filters, Stride, Padding
@@ -39,4 +40,88 @@ class CNNBlock(nn.Module):
         return self.leakyrelu(self.batchnorm(self.conv(x)))
 
 
-class Yolov1
+class Yolov1(nn.Module):
+    def __init__(self, in_channels=3, **kwargs):
+        super(Yolov1, self).__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        # Darknet by Joseph Redmund
+        self.darknet = self._create_conv_layers(self.architecture)
+        # Fully connected
+        self.fcs = self._create_fcs(**kwargs)
+
+    def forward(self, x):
+        x = self.darknet(x)
+        # we don't wat to flatten the number of examples
+        return self.fcs(torch.flatten(x, start_dim=1))
+
+    def _create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for x in architecture:
+            if type(x) == tuple:
+                layers += [CNNBlock(
+                    in_channels,
+                    x[1],
+                    kernel_size=x[0],
+                    stride=x[2],
+                    padding=x[3],
+                )]
+                in_channels = x[1]
+            elif type(x) == str:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            elif type(x) == list:
+                conv1 = x[0]  # Tuple
+                conv2 = x[1]  # Tuple
+                num_repeats = x[2]  # Integer
+
+                for _ in range(num_repeats):
+                    layers += [
+                        CNNBlock(
+                            in_channels,
+                            conv1[1],
+                            kernel_size=conv1[0],
+                            stride=conv1[2],
+                            padding=conv1[3],
+                        )
+                    ]
+
+                    layers += [
+                        CNNBlock(
+                            conv1[1],
+                            conv2[1],
+                            kernel_size=conv2[0],
+                            stride=conv2[2],
+                            padding=conv2[3],
+                        )
+                    ]
+
+                    in_channels = conv2[1]
+
+        return nn.Sequential(*layers)
+
+    def _create_fcs(self, split_size, num_boxes, num_classes):
+        S, B, C = split_size, num_boxes, num_classes
+
+        # In original paper this should be
+        # nn.Linear(1024*S*S, 4096),
+        # nn.LeakyReLU(0.1),
+        # nn.Linear(4096, S*S*(B*5+C))
+
+        return nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * S * S, 496),
+            nn.Dropout(0.0),
+            nn.LeakyReLU(0.1),
+            nn.Linear(496, S * S * (C + B * 5)),  # (S,S,30)
+        )
+
+
+def test(S=7, B=2, C=20):
+    #split_size=7, num_boxes=2, num_classes=20
+    model = Yolov1(split_size=S, num_boxes=B, num_classes=C)
+    x = torch.randn((2, 3, 448, 448))
+    print(model(x).shape)
+
+test()
